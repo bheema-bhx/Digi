795ea424e5adef13e5c2bcf6be412dc79a7fc52e
bheema-bhx <bheema23@yahoo.com>
2015-06-07 10:58:34 +0530

    initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <div class="container">
+        <p class="text-muted credit"><span style="text-align: left; float: left">&copy; 2015 <a href="#">DigiAp
+          </a></span> <span class="hidden-phone"
+                                                    style="text-align: right; float: right">Developed & Maintaining by: <a
+                        href="http://bhxpro.com/" alt="BHXPRO">BHXPRO</a></span></p>
+    </div>
+</div>
